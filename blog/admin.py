254371a25b53a1e96750f47492cfa8b1eeb86429
Prakhar Soni <prakhar-chandra.soni@capgemini.com>
2022-10-09 19:58:44 +0000

    Finish Introduction and Django Admin

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Tag, Post
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+  prepulated_fields = { "slug": ("title",  ) }
+  list_display = ["title", "published_at","author"]
+
+
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
