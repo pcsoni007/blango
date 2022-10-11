d20cdfe5d52e876319f32e91b8176a67fced38ca
Prakhar Soni <prakhar-chandra.soni@capgemini.com>
2022-10-11 17:51:32 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -20,4 +20,6 @@ import blog.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.index),
+    path("bootstrap/", blog.views.bootstrap),
+    path("post/<slug>/", blog.views.getPostDetails, name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.utils import timezone
 # Create your views here.
 def index(request):
     post = Post.objects.filter(published_at__lte=timezone.now())
     return render(request, "blog/index.html", { 'posts':post })
+
+def bootstrap(request):
+    return render(request, "blog/exbootstrap.html")
+
+def getPostDetails(request,slug):
+    post = get_object_or_404(Post, slug=slug)
+    print("Post ID", post.id, post.slug)
+    return render(request, "blog/post-detail.html", { "post":post })
